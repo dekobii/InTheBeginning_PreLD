f8a631217f3850c7d8b3a4eadd482e7d2ad851ee
Nicholas Thie <ntnicholasthie@gmail.com>
2018-04-20 09:47:50 +0700

    Added click test

---
+++ Assets/Scripts/PointAndClick.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointAndClick : MonoBehaviour {
+
+	void Update()
+	{
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit, 100f))
+			{
+				// Execute something on hit gameobject
+				GameObject hitGO = hit.transform.gameObject;
+				TileScript hitTile = hitGO.GetComponent<TileScript>();
+				hitTile.Grow();
+			}
+		}
+	}
+}
---
+++ Assets/Scripts/TileScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileScript : MonoBehaviour {
+
+	public void Grow()
+	{
+		transform.localScale *= 1.1f;
+	}
+}
