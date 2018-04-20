41e929aca619ed8dc78e714d8f77ff0b3238cee8
Nicholas Thie <ntnicholasthie@gmail.com>
2018-04-20 11:05:53 +0700

    Fixed point and click with the new grid

--- Assets/Scripts/TileScript.cs
+++ Assets/Scripts/TileScript.cs
@@ -4,9 +4,39 @@ using UnityEngine;
 
 public class TileScript : MonoBehaviour {
 
+	public Material normalMaterial;
+	public Material highlightedMaterial;
+
+	private bool hovered;
+
+	void Start()
+	{
+		GetComponent<Renderer>().material = normalMaterial;
+		hovered = false;
+	}
+
+	void Update()
+	{
+		// Check if tile is being hovered this frame
+		if (hovered)
+		{
+			GetComponent<Renderer>().material = highlightedMaterial;
+		}
+		else
+		{
+			GetComponent<Renderer>().material = normalMaterial;
+		}
+		hovered = false;
+	}
+
 	public void Grow()
 	{
-        Debug.Log("Clicked");
 		transform.localScale *= 1.1f;
+	}
+
+	// Called to indicate this tile is being hovered
+	public void TileIsHovered()
+	{
+		hovered = true;
 	}
 }
